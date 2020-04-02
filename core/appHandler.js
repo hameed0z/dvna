091590c97868e8b74274c59332a22fc67d2df962
Abdelhameed Muhammad <hamid.mu7mad@gmail.com>
2020-04-02 09:00:31 +0200

    Update appHandler.js

--- core/appHandler.js
+++ core/appHandler.js
@@ -285,3 +285,120 @@ module.exports.userSearch1 = function (req, res) {
 		})
 	})
 }
+
+module.exports.userSearch2 = function (req, res) {
+	var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
+	db.sequelize.query(query, {
+		model: db.User
+	}).then(user => {
+		if (user.length) {
+			var output = {
+				user: {
+					name: user[0].name,
+					id: user[0].id
+				}
+			}
+			res.render('app/usersearch', {
+				output: output
+			})
+		} else {
+			req.flash('warning', 'User not found')
+			res.render('app/usersearch', {
+				output: null
+			})
+		}
+	}).catch(err => {
+		req.flash('danger', 'Internal Error')
+		res.render('app/usersearch', {
+			output: null
+		})
+	})
+}
+
+module.exports.userSearch3 = function (req, res) {
+	var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
+	db.sequelize.query(query, {
+		model: db.User
+	}).then(user => {
+		if (user.length) {
+			var output = {
+				user: {
+					name: user[0].name,
+					id: user[0].id
+				}
+			}
+			res.render('app/usersearch', {
+				output: output
+			})
+		} else {
+			req.flash('warning', 'User not found')
+			res.render('app/usersearch', {
+				output: null
+			})
+		}
+	}).catch(err => {
+		req.flash('danger', 'Internal Error')
+		res.render('app/usersearch', {
+			output: null
+		})
+	})
+}
+
+module.exports.userSearch4 = function (req, res) {
+	var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
+	db.sequelize.query(query, {
+		model: db.User
+	}).then(user => {
+		if (user.length) {
+			var output = {
+				user: {
+					name: user[0].name,
+					id: user[0].id
+				}
+			}
+			res.render('app/usersearch', {
+				output: output
+			})
+		} else {
+			req.flash('warning', 'User not found')
+			res.render('app/usersearch', {
+				output: null
+			})
+		}
+	}).catch(err => {
+		req.flash('danger', 'Internal Error')
+		res.render('app/usersearch', {
+			output: null
+		})
+	})
+}
+
+
+module.exports.userSearch5 = function (req, res) {
+	var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
+	db.sequelize.query(query, {
+		model: db.User
+	}).then(user => {
+		if (user.length) {
+			var output = {
+				user: {
+					name: user[0].name,
+					id: user[0].id
+				}
+			}
+			res.render('app/usersearch', {
+				output: output
+			})
+		} else {
+			req.flash('warning', 'User not found')
+			res.render('app/usersearch', {
+				output: null
+			})
+		}
+	}).catch(err => {
+		req.flash('danger', 'Internal Error')
+		res.render('app/usersearch', {
+			output: null
+		})
+	})
+}
